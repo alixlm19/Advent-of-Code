2e1689341615c503a07574068e9f5cf70a986563
alixlm19 <alixlm19@hotmail.com>
2023-12-04 13:24:14 -0500

    Add read_lines function to utils module

---
+++ utils/utils.py
@@ -0,0 +1,8 @@
+def read_lines(file_name: str, data_dir_path: str = "./data", test = False):
+    file_name = f"test/{file_name}" if test else file_name
+    file_path: str = f"{data_dir_path}/{file_name}"
+    
+    lines: [str] = []
+    with open(file_path, 'r') as f:
+        lines = [line.removesuffix('\n') for line in f.readlines()]
+    return lines
